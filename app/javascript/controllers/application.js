1e25fb7357e037dd45315c27ab8f9828086725a7
michihiro kawaji <michikun06@icloud.com>
2022-03-20 18:50:06 +0900

    JSを読み込む手前まで

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -7,3 +7,10 @@ application.debug = false
 window.Stimulus   = application
 
 export { application }
+
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+require("jquery")
+import "bootstrap"
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  get 'sessions/new'
   root                   'static_pages#home'
   get      'contact'  => 'static_pages#contact'
   get      'about'    => 'static_pages#about'
   get      'signup'   => 'users#new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+  resources :users
 end
